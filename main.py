0b625a8f8bb6d78329a9a9b7ae2530a70d891fa8
szznv <120127927+szznv@users.noreply.github.com>
2024-04-25 02:38:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import streamlit as st
-from streamlit_option_menu import option_menu
 from PIL import Image
 from pathlib import Path
 
@@ -26,44 +25,54 @@ from pathlib import Path
 #     icons=['house', 'cloud-upload', "list-task", 'gear'], 
 #     menu_icon="cast", default_index=0, orientation="horizontal")
 # selected2
-
+#background-image:  linear-gradient(#868be4 2px, transparent 2px), linear-gradient(90deg, #868be4 2px, transparent 2px), linear-gradient(#868be4 1px, transparent 1px), linear-gradient(90deg, #868be4 1px, #e9e9f8 1px);
+#background-size: 50px 50px, 50px 50px, 10px 10px, 10px 10px;
+#background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
 page_bg_img = """
 <style>
 
-[data-testid="stAppViewContainer"] {      
+[data-testid="stAppViewContainer"] {     
+    background-color: #ffffff;
+    opacity: 0.8;
+    background-image:  repeating-radial-gradient( circle at 0 0, transparent 0, #ffffff 350px ), repeating-linear-gradient( #444cf755, #444cf7 );
 
 
-background-color: #e9e9f8;
-opacity: 1;
-background-image:  linear-gradient(#868be4 2px, transparent 2px), linear-gradient(90deg, #868be4 2px, transparent 2px), linear-gradient(#868be4 1px, transparent 1px), linear-gradient(90deg, #868be4 1px, #e9e9f8 1px);
-background-size: 50px 50px, 50px 50px, 10px 10px, 10px 10px;
-background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
-
-
-
-p { color: black;}
-}
+p { color: black;}}
 [data-testid="stHeader"]{                    
-background-color: rgba(0, 0, 0, 0);
-color: black;
-font-size: 50px;
+    background-color: rgba(0, 0, 0, 0);
+    color: black;
+    font-size: 100px;
+    font_weight: bold;
+    
+}
+[data-testid="stSideBar"]{                    
+    background-color: #819e66;
 }
 </style>
 """
-
-
-
 st.markdown(page_bg_img, unsafe_allow_html=True)
-# st.title(':gray[Найди профессию мечты]')
 
-text = "Найди профессию мечты"
-st.write(text)
+# st.title(':gray[Найди профессию мечты]')
+gradient_text_html = """
+<style>
+.gradient-text {
+    font-weight: bold;
+    background-color: black;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    display: inline;
+    font-size: 3em;
+}
+</style>
+<div class="gradient-text">Найди профессию мечты</div>
+"""
+
+st.markdown(gradient_text_html, unsafe_allow_html=True)
 
 nap = st.selectbox(
     'Выбери направление',
     ('Направление не выбрано','ИТ', 'Программирование', 'Аналитика', 'Машинное обучение', 'Тестирование', 'ИТ-архитектура', 'Blockchain', 'Продукт и проекты', 'Маркетинг', 'Игры', 'Дизайн'))
 tab1 = st.write('Ты выбрал:', nap)
-
 if nap == "ИТ":
     time = st.selectbox(
     'Сколько ты бы хотел учиться?',
@@ -118,7 +127,7 @@ if nap == "ИТ":
                                     ('Дополнительные цели не выбраны', 'Получить модную профессию', 'Самоопределение', 'Релокация заграницу', 'Фининсовая независимость'))
                                     tab2 = st.write('Ты выбрал:', dop)
                                     if dop!='Дополнительные цели не выбраны':
-                                        
+
                                         if st.button("Отправить данные"):
                                             data_to_send = {
                                                 "Направление": nap,
